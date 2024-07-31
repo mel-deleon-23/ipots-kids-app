ea5a9807ce32a4b0825c78c29f9b62b9d099e3b0
MEL-23-bit <69704719+MEL-23-bit@users.noreply.github.com>
2024-07-31 10:59:53 -0400

    created trivia folder for trivia components

--- ipots-kids/src/components/Trivia/TriviaSummary.jsx
+++ ipots-kids/src/components/Trivia/TriviaSummary.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styles/TriviaSummary.css';
+import '../Trivia/styles/TriviaSummary.css';
 
 const TriviaSummary = () => {
   const location = useLocation();
