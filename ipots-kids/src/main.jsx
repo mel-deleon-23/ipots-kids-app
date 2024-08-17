a4401eaebdaf3e0d6a56d1f9039ffe8ebe0e4bc0
Erko Abdurahman <erkoabdu@gmail.com>
2024-08-17 10:31:35 -0400

    Ederes and I added google sign in and article to explain in main homepage

--- ipots-kids/src/main.jsx
+++ ipots-kids/src/main.jsx
@@ -2,9 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // NOTE FROM ERKO AND EDERES:
+  // Replace clientId with your own client id from here https://console.cloud.google.com/apis/credentials
+  // follow this guide to setup using react https://muhammedsahad.medium.com/react-js-a-step-by-step-guide-to-google-authentication-926d0d85edbd
+  <GoogleOAuthProvider clientId="PUT YOUR CLIENT ID HERE">
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </GoogleOAuthProvider>
 );
--- ipots-kids/src/pages/HomePage.jsx
+++ ipots-kids/src/pages/HomePage.jsx
@@ -1,7 +1,42 @@
 import "../styles/signup/styles.css";
 import { Link } from "react-router-dom";
+import { googleLogout, useGoogleLogin } from "@react-oauth/google";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [user, setUser] = useState([]);
+  const [profile, setProfile] = useState([]);
+
+  const login = useGoogleLogin({
+    onSuccess: (codeResponse) => setUser(codeResponse),
+    onError: (error) => console.log("Login Failed:", error),
+  });
+
+  useEffect(() => {
+    if (user) {
+      axios
+        .get(
+          `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`,
+          {
+            headers: {
+              Authorization: `Bearer ${user.access_token}`,
+              Accept: "application/json",
+            },
+          }
+        )
+        .then((res) => {
+          setProfile(res.data);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [user]);
+
+  // log out function to log the user out of google and set the profile array to null
+  const logOut = () => {
+    googleLogout();
+    setProfile(null);
+  };
   return (
     <>
       <div className="container-fluid space">
@@ -14,16 +49,41 @@ export default function Home() {
             <Link to="/consent">
               <button className=" button-format buttonEmpty">Sign up</button>
             </Link>
-            <Link to="/google">
-              <button className=" button-format buttonGoogle d-flex flex-row justify-content-center align-items-center">
+            {/* <Link to="/google"> */}
+            {profile ? (
+              <div>
+                <img src={profile.picture} alt="user image" />
+                <h3>User Logged in</h3>
+                <p>Name: {profile.name}</p>
+                <p>Email Address: {profile.email}</p>
+                <br />
+                <br />
+                <button
+                  onClick={() => logOut()}
+                  className=" button-format buttonGoogle d-flex flex-row justify-content-center align-items-center"
+                >
+                  <img
+                    className="google"
+                    src={`/images/google.png`}
+                    alt="google"
+                  />{" "}
+                  Sign out
+                </button>
+              </div>
+            ) : (
+              <button
+                onClick={() => login()}
+                className=" button-format buttonGoogle d-flex flex-row justify-content-center align-items-center"
+              >
                 <img
                   className="google"
                   src={`/images/google.png`}
                   alt="google"
                 />{" "}
-                Sign up with Google
+                Sign in with Google
               </button>
-            </Link>
+            )}
+            {/* </Link> */}
           </div>
         </div>
       </div>
