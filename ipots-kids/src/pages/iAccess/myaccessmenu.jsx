2c637ce6fee20a5b93272c525fee12e2c86a6f4a
Lovepreet <gillpreetsingh35@gmail.com>
2024-08-30 13:41:33 -0400

    disable category in which there is no accommodation bookmarked

--- ipots-kids/src/pages/iAccess/myaccessmenu.jsx
+++ ipots-kids/src/pages/iAccess/myaccessmenu.jsx
@@ -61,144 +61,129 @@ useEffect(() => {
 }, [user]);
   
 
-  useEffect(() => {
-    // for fetching data from database when page loads 
-    const fetchmyAccessibilityCat = async () => {
-        try {
-            const params = {
-                method: 'Category',
-                userId: userId,
-                location: location
-            };
-            
-            // Checking if the user is coming from the medical page
-            if (medicalCondition) {
-                params.medicalCondition = medicalCondition;
-            }
-            
-            const url = host + '/ipots-kids-app/ipots-server/myAccessibility.php';
-            const response = await axios.get(url, { params });
-            
-            setMyAccessibilityCat(response.data);
-            console.log(myAccessibilityCat);
-            
-          } catch (error) {
-            console.error("Error fetching Accommodation:", error);
+useEffect(() => {
+  // for fetching data from database when page loads 
+  const fetchmyAccessibilityCat = async () => {
+      try {
+          const params = {
+              method: 'Category',
+              userId: userId,
+              location: location
+          };
+          
+          // Checking if the user is coming from the medical page
+          if (medicalCondition) {
+              params.medicalCondition = medicalCondition;
           }
-    };
-    fetchmyAccessibilityCat();
+          
+          const url = host + '/iPots/iAccess-Server/myAccessibility.php';
+          const response = await axios.get(url, { params });
+          
+          setMyAccessibilityCat(response.data.map(item => item.category));
+          // console.log(response.data.map(item => item.category));
+          
+        } catch (error) {
+          console.error("Error fetching Accommodation:", error);
+        }
+  };
+  fetchmyAccessibilityCat();
 }, [location]);
 
-  const handleLocationClick = (location) => {
-    setSelectedLocation(location);
-    fetchmyAccessibilityCat();
-  };
+const handleLocationClick = (location) => {
+  setSelectedLocation(location);
+  fetchmyAccessibilityCat();
+};
 
-  const checkBeforeNavigate = (category, event) => {
-    // checking if the user selected the location
-    if (!selectedLocation) {
-      alert("Please select a location first");
-      event.preventDefault(); // Prevent the default link click behavior
-      return;
-    }
-    let Url =
-      "/myaccommodations?location=" + selectedLocation + "&category=" + category;
+const checkBeforeNavigate = (category, event) => {
+  // checking if the user selected the location
+  if (!selectedLocation) {
+    alert("Please select a location first");
+    event.preventDefault(); // Prevent the default link click behavior
+    return;
+  }
+  let Url =
+    "/myaccommodations?location=" + selectedLocation + "&category=" + category;
 
-    if (medicalCondition) {
-      Url += `&medicalCondition=${medicalCondition}`;
-    }
+  if (medicalCondition) {
+    Url += `&medicalCondition=${medicalCondition}`;
+  }
 
-    navigate(Url);
-  };
+  navigate(Url);
+};
 
-  const locations = [
-    { name: "Home", img: homeImg },
-    { name: "Work", img: briefcaseImg },
-    { name: "School", img: backpackImg },
-    { name: "Transit", img: transitImg },
-    { name: "Medical", img: hospitalImg },
-    { name: "All", img: earthImg },
-  ];
+const locations = [
+  { name: "Home", img: homeImg },
+  { name: "Work", img: briefcaseImg },
+  { name: "School", img: backpackImg },
+  { name: "Transit", img: transitImg },
+  { name: "Medical", img: hospitalImg },
+  { name: "All", img: earthImg },
+];
 
-  const categories = [
-    { name: "Mobility", img: mobilityImg, grayImg: grayMobilityImg },
-    { name: "Hearing", img: earImg, grayImg: grayEarImg },
-    { name: "Cognitive", img: brainImg, grayImg: grayBrainImg },
-    { name: "Mental Health", img: mentalImg, grayImg: grayMentalImg},
-    { name: "Sensory", img: sensorImg, grayImg: graySensorImg},
-    { name: "Allergy", img: allergyImg, grayImg: grayAllergyImg},
-    { name: "Vision", img: visionImg, grayImg: grayVisionImg },
-    { name: "Pain", img: painImg, grayImg: grayPainImg },
-    { name: "Digestion", img: stomachImg, grayImg: grayStomachImg },
-    { name: "Safety", img: safetyImg, grayImg: graySafetyImg },
-    { name: "Medical Devices", img: medicalImg, grayImg: grayMedicalImg },
-    { name: "Medicine", img: medicationImg, grayImg: grayMedicationImg },
-  ];
+const categories = [
+  { name: "Mobility", img: mobilityImg, grayImg: grayMobilityImg },
+  { name: "Hearing", img: earImg, grayImg: grayEarImg },
+  { name: "Cognitive", img: brainImg, grayImg: grayBrainImg },
+  { name: "Mental Health", img: mentalImg, grayImg: grayMentalImg},
+  { name: "Sensory", img: sensorImg, grayImg: graySensorImg},
+  { name: "Allergy", img: allergyImg, grayImg: grayAllergyImg},
+  { name: "Vision", img: visionImg, grayImg: grayVisionImg },
+  { name: "Pain", img: painImg, grayImg: grayPainImg },
+  { name: "Digestion", img: stomachImg, grayImg: grayStomachImg },
+  { name: "Safety", img: safetyImg, grayImg: graySafetyImg },
+  { name: "Medical Devices", img: medicalImg, grayImg: grayMedicalImg },
+  { name: "Medicine", img: medicationImg, grayImg: grayMedicationImg },
+];
 
-  const displayNames = {
-    Mobility: "Mobility",
-    Hearing: "Hearing",
-    Cognitive: "Cognitive",
-    MentalHealth: "Mental Health",
-    Sensory: "Sensory",
-    Allergy: "Allergy",
-    Vision: "Vision",
-    Pain: "Pain",
-    Digestion: "Digestion",
-    Safety: "Safety",
-    MedicalDevices: "Medical Devices",
-  };
-
-  return (
-    <>
-      <div className="access-menu-page">
-        <h1 className="header-access-menu-title">
-         My Accessibility Categories
-        </h1>
-        {medicalCondition && (
-                    <h1 className="medical-condition"> {medicalCondition}</h1>
-                )}
-        <div className="navbar-access-menu-container">
-          {locations.map((location) => (
-            <div
-              key={location.name}
-              className={`location-access-menu ${
-                selectedLocation === location.name ? "selected" : ""
-              }`}
-              onClick={() => handleLocationClick(location.name)}
-            >
-              <img
-                src={location.img}
-                alt={location.name}
-                className="location-access-menu-img"
-              />
-              <span className="location-access-menu-name">{location.name}</span>
-            </div>
-          ))}
+return (
+  <div className="access-menu-page">
+    <h1 className="header-access-menu-title">
+      My Accessibility Categories
+    </h1>
+    {medicalCondition && (
+      <h1 className="medical-condition">{medicalCondition}</h1>
+    )}
+    <div className="navbar-access-menu-container">
+      {locations.map((loc) => (
+        <div
+          key={loc.name}
+          className={`location-access-menu ${
+            selectedLocation === loc.name ? "selected" : ""
+          }`}
+          onClick={() => handleLocationClick(loc.name)}
+        >
+          <img
+            src={loc.img}
+            alt={loc.name}
+            className="location-access-menu-img"
+          />
+          <span className="location-access-menu-name">{loc.name}</span>
         </div>
-        <div className="categories-access-menu-container">
-          {
-            categories.map((category) => (
-            
-            <div
-              key={category.name}
-              className="category-access-menu"
-              onClick={(event) => checkBeforeNavigate(category.name, event)}
-            >
-              <img
-                src={category.img}
-                alt={category.name}
-                className="category-access-menu-icon"
-              />
-              <span className="category-access-menu-names">
-                {category.name}
-              </span>
-            </div>
-          ))}
-        </div>
-      </div>
-    </>
-  );
+      ))}
+    </div>
+    <div className="categories-access-menu-container">
+      {categories.map((category) => {
+        const isCategoryAvailable = myAccessibilityCat.includes(category.name);
+        return (
+          <div
+            key={category.name}
+            className={`category-access-menu ${isCategoryAvailable ? "" : "category-access-gray-menu"}`}
+            onClick={(event) => isCategoryAvailable && checkBeforeNavigate(category.name, event)}
+          >
+            <img
+              src={isCategoryAvailable ? category.img : category.grayImg}
+              alt={category.name}
+              className="category-access-menu-icon"
+            />
+            <span className="category-access-menu-names">
+              {category.name}
+            </span>
+          </div>
+        );
+      })}
+    </div>
+  </div>
+);
 };
 
 export default AccessMenu;
